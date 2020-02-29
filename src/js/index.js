4c83506782ee2d6c41cc7a322e5844c9593b6db0
Andrey Brekotkin <brekot@ya.ru>
2020-03-01 02:08:31 +0300

    Первая страница (без адаптива)

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,15 @@ window.$ = $;*/
 
 $(function() {
 
+    $('.main-block__head').click(function(){
+
+        $(this).toggleClass('main-block__head_open');
+
+        $(this).next('.main-block__body').slideToggle(400, function(){
+
+            $(this).toggleClass('main-block__body_open');
+        });
+    });
+
     $('select').styler();
 });
