f61ca2865ab4cea14859c5f1a1ef8e647712a345
bas <bas@ya.ru>
2020-03-03 14:23:55 +0300

    Третья страница (без адаптива)

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import Swiper from 'swiper';
 
 import formstyler from './jquery.formstyler.min.js';
 
@@ -39,10 +40,60 @@ $(function() {
 
     // Стилизация элементов
     $('select').styler();
+    $('input[type="file"]').styler();
 
     // Замена placeholder в строке поиска
     if ($(document).width() < 500)
     {
         $('.form-search__input').attr('placeholder', 'Поиск');
     }
+
+    // Вкладки справа
+	$('.tab-right__link').click(function(){
+
+		$('.tab-right__link_active').removeClass('tab-right__link_active');
+		$(this).addClass('tab-right__link_active');
+
+		$('.tab-container__item_active').removeClass('tab-container__item_active');
+		$('.tab-container__item').eq($(this).index()).addClass('tab-container__item_active');
+	});
+
+    // Слайдер галерея
+    var sliderLength = $('.apartment-gallery__item').length;
+
+    $('#agCountSumm').text(sliderLength);
+
+    var sliderGallery = new Swiper('.apartment-gallery__slider', {
+        loop: true,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 15,
+        breakpoints: {
+            1100: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 15,
+            },
+            767: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                spaceBetween: 15
+            },
+        },
+        navigation: {
+            prevEl: '.apartment-gallery__prev',
+            nextEl: '.apartment-gallery__next',
+        },
+    });
+
+    $('#agCountRange').text('1-' + sliderGallery.loopedSlides);
+
+    sliderGallery.on('slideChange', function () {
+
+        let tmp = sliderGallery.realIndex + sliderGallery.loopedSlides;
+
+        if (tmp > sliderLength) tmp = tmp - sliderLength;
+
+        $('#agCountRange').text((sliderGallery.realIndex + 1) + '-' + tmp);
+    });
 });
